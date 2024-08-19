8aadca640bef5064dd4d26538f00cb2ddc51de5c
lee-dong-chan <dsongc3082@naver.com>
2024-08-20 08:36:23 +0900

    feat:test

--- mock-test/src/app.ts
+++ mock-test/src/app.ts
@@ -5,12 +5,12 @@ import todo from "./controllers/todo";
 
 const app: Express = express();
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://54.152.135.30"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["http://54.152.135.30"],
+//     credentials: true,
+//   })
+// );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api/todo", todo);
