3c6894ca521b86a4fd86187dc1a6b63a15d3c26a
lee-dong-chan <dsongc3082@naver.com>
2024-08-20 10:35:14 +0900

    feat:test

--- mock-test/src/app.test.ts
+++ mock-test/src/app.test.ts
@@ -7,7 +7,7 @@ config();
 
 describe("Test Express App", () => {
   test("Get / return Now Testing", async () => {
-    const response = await request(app).get("/api");
+    const response = await request(app).get("/todo/api");
     expect(response.status).toBe(200);
     expect(response.text).toBe("Now Testing");
   });
